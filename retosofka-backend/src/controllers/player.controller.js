21c14742d89e2c25f51911f7e740018f4eae0fe6
Andres Camilo <andresdm97@hotmail.com>
2022-04-26 20:09:33 -0500

    Documentación de las APIS

--- retosofka-backend/src/controllers/player.controller.js
+++ retosofka-backend/src/controllers/player.controller.js
@@ -1,5 +1,6 @@
 const Player = require("../models/player.model");
 
+// Este controlador crea un jugador nuevo con su nombre de usuario
 module.exports.createPlayer = async (req, res) => {
     try {
         const { username, score } = req.body;
@@ -16,14 +17,14 @@ module.exports.createPlayer = async (req, res) => {
         return res.status(500).json(error);
     }
 };
-
+// Este controlador busca al jugador que ingresó en el concurso 
 module.exports.getPlayerByUsername = async (req, res) => {
     const { usernamePlayer } = req.params;
     const player = await Player.findOne({ username: usernamePlayer });
     return res.json(player);
   
   };
-
+//Este controlador actualiza el puntaje de cada jugador según se desempeña en el concurso
   module.exports.updatePlayerByusername = async (req, res) => {
     const {filter, update } = req.params;
     const opts = { new: true };
